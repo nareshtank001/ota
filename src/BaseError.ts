4ffec2ce20f480fe1cacdccecbfd49f3709ab8b8
Naresh Tank <tanknaresh7@gmail.com>
2024-12-19 16:42:58 +0530

    Update BaseError.ts

--- src/BaseError.ts
+++ src/BaseError.ts
@@ -1,5 +1,7 @@
 export class BaseError extends Error {
     constructor(name: string, message: string, stack?: any) {
+ this.name = name;
+        this.stack = stack;
         super(message);
         this.name = name;
         this.stack = stack;
